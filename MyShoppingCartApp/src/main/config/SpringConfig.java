58a477e9a5228be80e201daa5a83bc2607206e2e
ricardoag <ricardo@enmotionvalue.com>
2025-03-12 10:17:43 +0100

    updaye

---
+++ MyShoppingCartApp/src/main/config/SpringConfig.java
@@ -0,0 +1,12 @@
+package com.myshoppingcart.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ComponentScan(basePackages = {"com.myshoppingcart.persistence", "com.myshoppingcart.service"})
+@PropertySource("classpath:config.properties")
+public class SpringConfig {
+}
