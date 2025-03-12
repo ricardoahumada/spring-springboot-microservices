f7b96a60c82a344684501d47055e3fcb4ee6901e
ricardovm <ricardo@enmotionvalue.com>
2025-03-12 17:01:43 +0100

    update

--- SchoolManager-JPA/src/main/java/com/banana/models/Project.java
+++ SchoolManager-JPA/src/main/java/com/banana/models/Project.java
@@ -11,9 +11,10 @@ import java.util.List;
 @AllArgsConstructor
 @ToString
 @Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public class Project {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
 
