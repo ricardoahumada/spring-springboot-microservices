7379e12bc6c62d91dd3df7f9bb7a57d5d4921ae0
ricardovm <ricardo@enmotionvalue.com>
2025-03-12 10:56:50 +0100

    update

--- MyShoppingCartApp/src/test/java/com/myshoppingcart/AppTest.java
+++ MyShoppingCartApp/src/test/java/com/myshoppingcart/AppTest.java
@@ -1,15 +1,27 @@
 package com.myshoppingcart;
 
+import com.myshoppingcart.config.SpringConfig;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {SpringConfig.class})
 class AppTest {
+
+    @Autowired
+    ApplicationContext ctx;
 
     @Test
     void loadContext() {
         assertTrue(true);
+        assertNotNull(ctx);
     }
 
 }
