70379017eea42c3742ba6d7ebe8912d7e596bfd5
ricardovm <ricardo@enmotionvalue.com>
2025-03-18 16:34:18 +0100

    update

--- springboot/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
+++ springboot/ProductsService/src/main/java/com/microcompany/productsservice/controller/ProductServiceController.java
@@ -1,10 +1,36 @@
 package com.microcompany.productsservice.controller;
 
+import com.microcompany.productsservice.model.Product;
+import com.microcompany.productsservice.model.StatusMessage;
+import com.microcompany.productsservice.persistence.ProductsRepository;
+import com.microcompany.productsservice.service.ProductsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductServiceController {
     private static final Logger logger = LoggerFactory.getLogger(ProductServiceController.class);
 
+    @Autowired
+    private ProductsService servicioProds;
+
+    @Autowired
+    private ProductsRepository productsRepository;
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ResponseEntity getAllProducts(){
+        List<Product> products= productsRepository.findAll();
+        if(products!=null && products.size()>0) return ResponseEntity.status(HttpStatus.OK).body(products);
+        else return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new StatusMessage(HttpStatus.NOT_FOUND, "No se han encontrado productos"));
+    }
 
 }
--- springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
+++ springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 // TODO: uncomment and implement methods
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@Sql("classpath:data.sql")
+//@Sql("classpath:_data.sql_")
 public class ProductServiceTestRestTemplate {
     // @Value(value = "${local.server.port}")
     @LocalServerPort
