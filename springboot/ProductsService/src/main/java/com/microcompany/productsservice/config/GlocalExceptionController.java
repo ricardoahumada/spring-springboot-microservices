daa014579cf08c83454a083a9f36fe2e94bc767e
ricardovm <ricardo@enmotionvalue.com>
2025-03-19 12:30:57 +0100

    update

--- springboot/ProductsService/src/main/java/com/microcompany/productsservice/config/GlocalExceptionController.java
+++ springboot/ProductsService/src/main/java/com/microcompany/productsservice/config/GlocalExceptionController.java
@@ -6,6 +6,7 @@ import com.microcompany.productsservice.exception.ProductNotfoundException;
 import com.microcompany.productsservice.model.StatusMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
