6ca47bad0e491e2ef7ea9bcb1571f5bd014579bf
ricardovm <ricardo@enmotionvalue.com>
2025-03-21 08:45:13 +0100

    update

--- springboot/ProductsService/src/main/java/com/microcompany/productsservice/model/Product.java
+++ springboot/ProductsService/src/main/java/com/microcompany/productsservice/model/Product.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.Size;
 
 import com.microcompany.productsservice.contraints.SerialNumber;
 import com.microcompany.productsservice.exception.GlobalProductException;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Entity
@@ -14,9 +15,11 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Schema(name = "Producto", description = "El producto")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(name = "Id", description = "El identificador del producto", example = "345", format = "int32")
     private Long id;
 
     @NotBlank
