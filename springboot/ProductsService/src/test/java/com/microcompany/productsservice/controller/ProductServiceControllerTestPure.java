21bc50964a435c0bdd594670d2ea67a9b2d88b30
ricardovm <ricardo@enmotionvalue.com>
2025-03-24 15:25:55 +0100

    update testing

--- springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceControllerTestPure.java
+++ springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceControllerTestPure.java
@@ -1,7 +1,18 @@
 package com.microcompany.productsservice.controller;
 
+import com.microcompany.productsservice.exception.ProductNotfoundException;
 import com.microcompany.productsservice.model.Product;
+import com.microcompany.productsservice.persistence.ProductsRepository;
+import com.microcompany.productsservice.service.ProductsService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
@@ -11,20 +22,59 @@ import org.springframework.test.context.jdbc.Sql;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 // TODO: uncomment and implement methods
-//@SpringBootTest
-@Sql(value = "classpath:testing.sql")
+@ExtendWith(MockitoExtension.class) // más puro
+@MockitoSettings(strictness = Strictness.LENIENT) // más puro
 class ProductServiceControllerTestPure {
 
-    
+    @BeforeEach
+    public void setUp() {
+
+        List<Product> productsFake = List.of(
+                new Product(1l, "Fake prod 1", "111-222-3333"),
+                new Product(2l, "Fake prod 2", "111-222-4444"),
+                new Product(3l, "Fake prod 3", "111-222-555")
+        );
+
+        Mockito.when(prodServiceMock.getProductsByText("Fake")).thenReturn(productsFake);
+        Mockito.when(prodServiceMock.getProductsByText("a")).thenThrow(new ProductNotfoundException("No hay productos mock"));
+
+        Mockito.when(prodServiceMock.create(Mockito.any(Product.class)))
+                .thenAnswer(elem -> {
+                    Product ap = (Product) elem.getArguments()[0];
+                    ap.setId(100L);
+                    return ap;
+                });
+    }
+
+    @InjectMocks // más puro
     private ProductServiceController controller;
 
+    @Mock
+    private ProductsService prodServiceMock;
+
+    @Mock
+    ProductsRepository productsRepositoryMock;
+
+
     @Test
-    void givenProducts_whengetAllProducts_thenIsNotNull() {
+    void givenProducts_whengetAllProductsGoodSearch_thenOK() {
         ResponseEntity<List<Product>> response = controller.getAllProducts("");
         assertThat(response.getStatusCode().value()).isEqualTo(HttpStatus.OK.value());
         assertThat(response.getBody()).isNotNull();
+
+        List<Product> prods = response.getBody();
+        System.out.println("Prods: " + prods);
+    }
+
+    @Test
+    void givenProducts_whengetAllProductsBadSearch_thenNOTFOUND() {
+        assertThatExceptionOfType(ProductNotfoundException.class).isThrownBy(() -> {
+            ResponseEntity<List<Product>> response = controller.getAllProducts("a");
+        });
+
     }
 
     @Test
--- springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceControllerTest_MockMvc.java
+++ springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceControllerTest_MockMvc.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.hamcrest.Matchers.hasItem;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,21 +25,26 @@ import static org.hamcrest.Matchers.is;
 
 
 // TODO: uncomment and implement methods
-// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-// @AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 // @TestPropertySource( locations = "classpath:application-integrationtest.properties")
+@Sql(value = "classpath:testing.sql")
 class ProductServiceControllerTest_MockMvc {
+
     @Autowired
-    private ProductsRepository repository;
+    private MockMvc mvc;
 
     @Test
     public void givenProducts_whenGetProducts_thenStatus200() throws Exception {
+        mvc.perform(get("/products").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name", is("Prod Test 1")));
+    }
 
-    
-}
     @Test
     void givenProducts_whenValidCreateProduct_thenIsCreatedAndHaveId() throws Exception {
 
     }
-    
+
 }
