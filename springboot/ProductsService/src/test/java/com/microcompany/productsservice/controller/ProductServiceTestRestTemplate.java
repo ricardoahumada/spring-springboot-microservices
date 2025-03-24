8f9da592ad3ef7e4fb5b88e16dee12711e183891
ricardovm <ricardo@enmotionvalue.com>
2025-03-24 16:59:12 +0100

    update testing

--- springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
+++ springboot/ProductsService/src/test/java/com/microcompany/productsservice/controller/ProductServiceTestRestTemplate.java
@@ -3,6 +3,7 @@ package com.microcompany.productsservice.controller;
 import com.microcompany.productsservice.model.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -12,25 +13,46 @@ import org.springframework.test.context.jdbc.Sql;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 // TODO: uncomment and implement methods
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@Sql("classpath:data.sql")
+@Sql("classpath:testing.sql")
 public class ProductServiceTestRestTemplate {
-    // @Value(value = "${local.server.port}")
-    @LocalServerPort
+    @Value(value = "${local.server.port}")
+//    @LocalServerPort
     private int port;
+
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
     public void givenUrl_whenGetProducts_thenAStringExists() throws Exception {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/products", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        System.out.println(response.getBody());
+
+        assertThat(response.getBody()).contains("Prod Test");
+        assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
+
     }
 
     @Test
     public void givenUrl_whenGetProducts_thenAProductExists() throws Exception {
+        HttpHeaders header = new HttpHeaders();
+        header.set("ACCEPT", MediaType.APPLICATION_JSON_VALUE);
+        ResponseEntity<Product[]> response = restTemplate.getForEntity("http://localhost:" + port + "/products", Product[].class, header);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getHeaders().getContentType()).isEqualTo(MediaType.APPLICATION_JSON);
+
+        System.out.println(response.getBody());
+        assertThat(response.getBody()).extracting(Product::getName).contains("Prod Test 1");
+
     }
 
     @Test
