7046abfe364db9c9167a3bdcd08d07fc4f6b4848
ricardovm <ricardo@enmotionvalue.com>
2025-03-26 10:15:34 +0100

    update sec

--- security/basic/DemoSpringSecurityApplication/src/main/java/com/microcompany/demo/security/SecurityConfig.java
+++ security/basic/DemoSpringSecurityApplication/src/main/java/com/microcompany/demo/security/SecurityConfig.java
@@ -7,14 +7,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 // TODO: uncomment
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
-    @Bean
+//    @Bean
     public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
         return http.getSharedObject(AuthenticationManagerBuilder.class)
                 .build();
@@ -23,6 +24,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                (request)->request
+//                        .anyRequest().permitAll()
+                        .antMatchers("/auth/login").permitAll()
+                        .anyRequest().authenticated()
+        ).httpBasic();
 
         return http.build();
     }
